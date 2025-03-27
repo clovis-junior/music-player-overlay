79547357cc2d2273ed32348496f014f0fb4971f7
Clovis Junior <clovis.sa.junior@gmail.com>
2025-03-27 00:33:51 -0300

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -1,12 +1,17 @@
 export function AddZero(number = 0) {
-    number = number.toString();
+  number = number.toString();
   
-    return (number.length < 2) ? `0${number.toString()}` : number
+  return (number.length < 2) ? `0${number.toString()}` : number
+}
+
+export function URLValidade(value) {
+  let filter=/^(https?)\:\/\/(?:www\.)?([a-zA-Z0-9-]{1,256})\.([a-zA-Z0-9.]{2,})\b([a-zA-Z0-9-_()@:%\+.~#?&\/\/=]*)$/is;
+  return (filter.test(value)) ? true : false;
 }
   
 export function ConvertTime(time = 0) {
-    const minutes = Math.floor(time / 60),
-      seconds = (time % 60);
+  const minutes = Math.floor(time / 60),
+    seconds = (time % 60);
   
-    return `${AddZero(minutes)}:${AddZero(seconds)}`
+  return `${AddZero(minutes)}:${AddZero(seconds)}`
 }
