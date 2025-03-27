e9f6282848f7060e1bf9ef7036993240b6849c2f
Clovis Junior <clovis.sa.junior@gmail.com>
2025-03-27 00:35:40 -0300

    Update AppleMusic.js

--- src/platforms/AppleMusic.js
+++ src/platforms/AppleMusic.js
@@ -1,35 +1,43 @@
 // import songData from'../Apple.test.json';
-const url = '//192.168.15.152:10767';
+import { URLValidade } from '../Utils';
+const url = '//127.0.0.1:10767';
 
-function GetAlbumCover(url, size = 300) {
-  const cover = url.replace('{w}', size)
-      .replace('{h}', size);
+function GetAlbumCover(url, size) {
+    if(URLValidade(url))
+        return '';
 
-  return cover
+    const cover = url.replace('{w}', size)
+    .replace('{h}', size);
+
+    return cover
 }
 
 export async function GetDataFromAppleMusic() {
-  try {
-    const response = await fetch(`${url}/currentPlayingSong`);
-  
-    if(response.status !== 200)
-      return { error: 'Access denied or invalid' };
-    
-    const data = await response.json() /*songData*/;
+    try {
+        const response = await fetch(`${url}/currentPlayingSong`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-    const isPlaying = !data.info?.isPlaying;
-    const title = data.info?.name;
-    const artist = data.info?.artistName;
-    const duration = {
-      elapsed: parseInt(data.info?.currentPlaybackTime) || 0,
-      percentage: (data.info?.currentPlaybackProgress * 100),
-      remaining: parseInt((data.info?.durationInMillis * 100) - data.info?.currentPlaybackTime),
-      total: parseInt(data.info?.durationInMillis / 1000) || 0
-    };
-    const albumCover = GetAlbumCover(data.info?.artwork.url, data.info?.artwork.width) || '';
-    
-    return {isPlaying, title, artist, duration, albumCover}
-  } catch(error) {
-    return { error: error.message.toString() };
-  }
+        if(response.status !== 200)
+            return { error: 'Access denied or invalid' };
+
+        const data = await response.json() /*songData*/;
+
+        const isPlaying = !data.info?.isPlaying;
+        const title = data.info?.name;
+        const artist = data.info?.artistName;
+        const duration = {
+            elapsed: parseInt(data.info?.currentPlaybackTime) || 0,
+            percentage: (data.info?.currentPlaybackProgress * 100),
+            remaining: parseInt((data.info?.durationInMillis * 100) - data.info?.currentPlaybackTime),
+            total: parseInt(data.info?.durationInMillis / 1000) || 0
+        };
+        const albumCover = GetAlbumCover(data.info?.artwork.url, (data.info?.artwork.width || 300));
+
+        return {isPlaying, title, artist, duration, albumCover}
+    } catch(error) {
+        return { error: error.message.toString() }
+    }
 }
