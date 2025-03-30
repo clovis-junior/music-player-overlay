fdd8e0aa969cf957bbeb2a0d2eb9effc4c42082d
clovis-junior <clovis.sa.junior@gmail.com>
2025-03-30 19:07:37 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     return (
         <Routes>
             <Route path='/' element={<PlayerPage />} />
+            <Route path='/player' element={<PlayerPage />} />
         </Routes>
     )
 }
