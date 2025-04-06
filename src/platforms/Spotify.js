e0c2da1a28b0e5ba2106b6b672287c8136629227
clovis-junior <clovis.sa.junior@gmail.com>
2025-04-06 19:23:17 -0300

    modified:   src/App.js 	modified:   src/platforms/Spotify.js

--- src/platforms/Spotify.js
+++ src/platforms/Spotify.js
@@ -1,5 +1,5 @@
 // import songData from'../Spotify.test.json';
-const inDevelopment = (window.location.host.match(/(localhost(:[0-9]{1,5})?)||(127\.0\.0\.1)/i));
+const inDevelopment = (/(localhost(:[0-9]{1,5})?)||(127\.0\.0\.1)/i.test(window.location.host));
 
 const params = new URLSearchParams(window.location.hash.split('?')[1]);
 
