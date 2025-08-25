e2383df2f7c0b9a8b2269bb9011d378bc4ea67e5
Clovis Junior <clovis.sa.junior@gmail.com>
2025-08-25 00:39:01 -0300

    Update Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -159,7 +159,8 @@ export function Player(props) {
         }
 
         return () => clearInterval(check);
-      } else if (!loaded || result?.error) {
+      } else if ((!loaded && !IsEmpty(result)) ||
+                (loaded && result?.error)) {
         console.log('Trying to get Spotify data...');
         const check = setInterval(async () => await GetResult(), 5000);
         return () => clearInterval(check);
