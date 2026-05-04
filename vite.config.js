2c8b9ad5fc47af5661c4a8d656fdb93b813bcb47
clovis-junior <clovis.sa.junior@gmail.com>
2026-05-04 12:24:36 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
-// import netlify from '@netlify/vite-plugin'
-import react from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  // build: {
-  //   minify: false,
-  //   sourcemap: true
-  // },
   plugins: [
-    // netlify(),
-    react()
+    netlify(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
   ],
 })
