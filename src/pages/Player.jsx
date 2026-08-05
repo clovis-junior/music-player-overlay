5af628900f4fbf713e405feb6480a224e1de3606
Clovis Junior <clovis.sa.junior@gmail.com>
2026-08-04 21:45:07 -0300

    Update Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -49,7 +49,7 @@ export default function Plugin() {
     if (!selectedFont) return;
 
     const formattedFont = selectedFont.replace(/\s+/g, '+');
-    const fontUrl = `https://fonts.googleapis.com/css2?family=${formattedFont}:wght@100..900&display=swap`;
+    const fontUrl = `https://fonts.googleapis.com/css2?family=${formattedFont}:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap`;
 
     let link = document.querySelector(`link[href="${fontUrl}"]`);
 
